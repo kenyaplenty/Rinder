c9d46028589266695a667373ef3c3b531cbe8364
Francesco Prospato <fprospato@gmail.com>
2020-11-12 17:19:01 -0800

    Add helper

---
+++ Rinder/RestaurantHelper.swift
@@ -0,0 +1,16 @@
+//
+//  RestaurantHelper.swift
+//  Rinder
+//
+//  Created by Francesco Prospato on 11/12/20.
+//
+
+import Foundation
+
+class RestaurantHelper {
+    
+    static func getCategories() {
+        var endPointString = "https://opentdb.com/api.php"
+    }
+    
+}
