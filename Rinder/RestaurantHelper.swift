7e3aadb3968ad13ef501ed3a4af2b5c6ee9d38b6
Francesco Prospato <fprospato@gmail.com>
2020-11-13 19:03:25 -0800

    Be able to grab categories

--- Rinder/RestaurantHelper.swift
+++ Rinder/RestaurantHelper.swift
@@ -6,11 +6,35 @@
 //
 
 import Foundation
+import Alamofire
 
 class RestaurantHelper {
     
-    static func getCategories() {
-        var endPointString = "https://opentdb.com/api.php" //zomato
-    }
+    private static let apiKey = "8a0c5aa6e710c35ba61ebef8de744a60"
     
+    static func getCategories(completionHandler: @escaping (_ receivedCategories: Bool) -> Void) {
+        
+        guard let url = URL(string: "https://developers.zomato.com/api/v2.1/categories") else {
+            return completionHandler(false)
+        }
+        
+        let headers: HTTPHeaders = [
+            "Accept": "application/json",
+             "user-key": apiKey
+        ]
+        
+        AF.request(url, method: .get, headers: headers).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                guard let dictionary = value as? [String: Any], response.response?.statusCode == 200 else {
+                    return completionHandler(false)
+                }
+                
+                return completionHandler(true)
+                
+            case .failure(let error):
+                return completionHandler(false)
+            }
+        }
+    }
 }
