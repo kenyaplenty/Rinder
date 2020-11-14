710c442d25adc98479af37383444a839a524fc99
Francesco Prospato <fprospato@gmail.com>
2020-11-13 20:21:30 -0800

    Be able to fetch restaurants around a user's location

--- Rinder/HomeVC.swift
+++ Rinder/HomeVC.swift
@@ -16,6 +16,7 @@ class HomeVC: UIViewController {
     
     //MARK: - Variables
     private var locationManager = CLLocationManager()
+    private var fetchingRestaurants = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -40,6 +41,16 @@ class HomeVC: UIViewController {
         locationManager.startUpdatingLocation()
     }
 
+    private func getRestaurants(lat: CLLocationDegrees, long: CLLocationDegrees) {
+        if !fetchingRestaurants {
+            fetchingRestaurants = true
+            RestaurantHelper.getRestaurants(lat: lat.binade,
+                                            long: long.binade) { (restuarants) in
+                print("got it!")
+                self.fetchingRestaurants = false
+            }
+        }
+    }
 }
 
 //MARK: - CLLocationManagerDelegate
@@ -47,7 +58,7 @@ class HomeVC: UIViewController {
 extension HomeVC: CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.last else { return }
-        
-        locationLbl.text = "lat: \(location.coordinate.latitude) long: \(location.coordinate.longitude)"
+        getRestaurants(lat: location.coordinate.latitude,
+                       long: location.coordinate.longitude)
     }
 }
--- Rinder/RestaurantHelper.swift
+++ Rinder/RestaurantHelper.swift
@@ -12,6 +12,8 @@ class RestaurantHelper {
     
     private static let apiKey = "8a0c5aa6e710c35ba61ebef8de744a60"
     
+    //MARK: - Getters
+    
     static func getCategories(completionHandler: @escaping (_ receivedCategories: Bool) -> Void) {
         
         guard let url = URL(string: "https://developers.zomato.com/api/v2.1/categories") else {
@@ -35,6 +37,100 @@ class RestaurantHelper {
             case .failure(let error):
                 return completionHandler(false)
             }
+        }
+    }
+    
+    static func getRestaurants(lat: Double,
+                               long: Double,
+                               completionHandler: @escaping (_ restaurants: [Restaurant]) -> Void) {
+        guard let url = URL(string: "https://developers.zomato.com/api/v2.1/search") else {
+            return completionHandler([])
+        }
+
+        let headers: HTTPHeaders = [
+            "Accept": "application/json",
+            "user-key": apiKey,
+            "lat": "\(lat)",
+            "lon": "\(long)",
+            "radius": "300", //radius is in meters
+            "sort" : "real_distance"
+        ]
+        
+        AF.request(url, method: .get, headers: headers).responseJSON { response in
+            switch response.result {
+            case .success(let value):
+                var restaurants = [Restaurant]()
+                
+                guard let dictionary = value as? [String: Any],
+                      response.response?.statusCode == 200,
+                      let restaurantsJSON = dictionary["restaurants"] as? [AnyObject] else {
+                    return completionHandler(restaurants)
+                }
+                
+                for restaurantJSON in restaurantsJSON {
+                    if let restaurantDict = restaurantJSON as? NSDictionary,
+                       let restaurantDetailsDict = restaurantDict["restaurant"] as? NSDictionary {
+                        restaurants.append(Restaurant(from: restaurantDetailsDict))
+                    }
+                }
+                
+                return completionHandler(restaurants)
+                
+            case .failure(let error):
+                return completionHandler([])
+            }
+        }
+    }
+}
+
+struct Restaurant {
+    var id: String = ""
+    var name: String = ""
+    var imageURL: String = ""
+    var url: String = ""
+    var address: String = ""
+    var latitude: String = ""
+    var longitude: String = ""
+    var cuisines: String = ""
+    var price_range: String = ""
+    
+    init(from jsonDict: NSDictionary) {
+        if let id = jsonDict["id"] as? String {
+            self.id = id
+        }
+        
+        if let name = jsonDict["name"] as? String {
+            self.name = name
+        }
+        
+        if let featured_image = jsonDict["featured_image"] as? String {
+            self.imageURL = featured_image
+        }
+        
+        if let url = jsonDict["url"] as? String {
+            self.url = url
+        }
+        
+        if let location = jsonDict["location"] as? NSDictionary {
+            if let address = location["address"] as? String {
+                self.address = address
+            }
+            
+            if let lat = location["latitude"] as? String {
+                self.latitude = lat
+            }
+            
+            if let lon = location["longitude"] as? String {
+                self.longitude = lon
+            }
+        }
+        
+        if let cuisines = jsonDict["cuisines"] as? String {
+            self.cuisines = cuisines
+        }
+        
+        if let price_range = jsonDict["price_range"] as? String {
+            self.price_range = price_range
         }
     }
 }
