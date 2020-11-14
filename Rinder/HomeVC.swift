5e6e68aeea01c2bfbb012a767be4f543b3d2b17c
Francesco Prospato <fprospato@gmail.com>
2020-11-13 19:02:54 -0800

    Be able to get and show user's current location

--- Rinder/HomeVC.swift
+++ Rinder/HomeVC.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import CoreLocation
 
 class HomeVC: UIViewController {
 
     //MARK: - Outlets
+    @IBOutlet weak var fromLbl: UILabel!
+    @IBOutlet weak var locationLbl: UILabel!
+    
+    //MARK: - Variables
+    private var locationManager = CLLocationManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupView()
+        getUserLocation()
+    }
+    
+    private func setupView() {
+        fromLbl.text = "Restaurants near"
+        fromLbl.font = UIFont.systemFont(ofSize: 18, weight: .regular)
+        fromLbl.textAlignment = .center
         
+        locationLbl.textAlignment = .center
+        locationLbl.text = "Current Location"
+        locationLbl.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+    }
+    
+    private func getUserLocation() {
+        locationManager.delegate = self
+        locationManager.requestAlwaysAuthorization()
+        locationManager.startUpdatingLocation()
     }
 
+}
+
+//MARK: - CLLocationManagerDelegate
+
+extension HomeVC: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        
+        locationLbl.text = "lat: \(location.coordinate.latitude) long: \(location.coordinate.longitude)"
+    }
 }
