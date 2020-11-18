25831c6045fa980622a7f24dc98f17843264240f
thengo1 <tbn2109@columbia.edu>
2020-11-17 20:05:10 -0500

    Adding test for getRestaurants

--- RinderTests/RinderTests.swift
+++ RinderTests/RinderTests.swift
@@ -4,6 +4,7 @@
 //
 //  Created by Francesco Prospato on 11/12/20.
 //
+// swiftlint:disable all
 
 import XCTest
 @testable import Rinder
@@ -18,16 +19,19 @@ class RinderTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    // Test getRestaurants
+    func testGetRestaurants() throws {
+        let vc = HomeVC()
+        vc.getRestaurants(latitude: 40.7128, longitude: 74.0060) // passes New York coordinates
+        XCTAssertTrue(vc.fetchingRestaurants)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
+    
+    // Test updateViewWithRestaurant
+    //func testUpdateViewWithRestaurant() throws {
+    //    let vc = HomeVC()
+    //    let restaurant = Restaurant.init(name: "Potato", cuisines: "Thai")
+    //    XCTAssertNotNil(restaurant)
+    //    vc.updateViewWithRestaurant(restaurant: restaurant!)
+    //    XCTAssertEqual(vc.titleLbl.text, restaurant?.name)
+    //}
 }
